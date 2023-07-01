394a82ebac369d92d3a8018db9f87f4dfa14669e
ToBinio <tobias.frischmann1@gmail.com>
2023-07-01 12:58:44 +0200

    parse deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use anyhow::{anyhow, bail};
+use serde::Deserialize;
+use toml::{Table, Value};
+
+#[derive(Deserialize)]
+struct Document {
+    dependencies: Table,
+}
+
+fn main() -> anyhow::Result<()> {
+    let string = fs::read_to_string("Cargo.toml")?;
+
+    let document: Document = toml::from_str(string.as_str())?;
+
+    for (name, value) in document.dependencies {
+        let result: anyhow::Result<String> = match value {
+            Value::String(version) => Ok(version),
+            Value::Table(version) => {
+                let version = version.get("version").ok_or(anyhow!("no"))?;
+
+                match version {
+                    Value::String(version) => Ok(version.to_string()),
+                    _ => bail!("invalid Cargo.toml"),
+                }
+            }
+            _ => bail!("invalid Cargo.toml"),
+        };
+
+        let version = result?;
+
+        println!("{} {}", name, version);
+    }
+
+    Ok(())
 }
